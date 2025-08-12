4322b3cc2d3f795cdf01aa2b438fd50d309ace7a
Saumya Sakshi <saumyasakshi41@gmail.com>
2025-08-12 10:13:04 +0530

    Initial commit

---
+++ Section 2/demo/src/main/java/com/saumya/demo/SpringbootFirstApplication.java
@@ -0,0 +1,23 @@
+package com.saumya.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringbootFirstApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringbootFirstApplication.class, args);
+
+
+		Alien a = context.getBean(Alien.class);
+		a.code();
+
+//		Laptop laptop= context.getBean(Laptop.class);
+//		laptop.compile();
+	}
+
+}
