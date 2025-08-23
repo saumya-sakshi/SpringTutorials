72c5a393142eb4d6c220eb1a53a3ed53ffac07cc
Saumya Sakshi <saumyasakshi41@gmail.com>
2025-08-23 16:39:06 +0530

    Responding to the client

--- Section 7/ServeletEx/src/main/java/org/example/HelloServlet.java
+++ Section 7/ServeletEx/src/main/java/org/example/HelloServlet.java
@@ -5,6 +5,8 @@ package org.example;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
 public class HelloServlet extends HttpServlet {
@@ -13,8 +15,11 @@ public class HelloServlet extends HttpServlet {
      * Whenever you want your servlet to work you need define a method service()
      */
 
-    public void service(HttpServletRequest req, HttpServletResponse res) {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In Service");
+        res.setContentType("text/html");
+       PrintWriter out= res.getWriter();
+       out.println("<H1><b>Hello, How are you?<b></H1>");
     }
 
 }
