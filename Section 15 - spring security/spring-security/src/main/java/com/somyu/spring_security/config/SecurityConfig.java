c8f0e1585484edcdad60ee5d862326ac97dd8db4
Saumya Sakshi <saumyasakshi41@gmail.com>
2025-11-22 18:48:39 +0530

    HArdcoding users in Config using InMemoryUserDetailsManager

--- Section 15 - spring security/spring-security/src/main/java/com/somyu/spring_security/config/SecurityConfig.java
+++ Section 15 - spring security/spring-security/src/main/java/com/somyu/spring_security/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,17 +19,33 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //lambda approach
-        /** To diable CSRF token**/
-        http.csrf(custom->custom.disable());
-        /** To Authorize all the requests**/
-        http.authorizeHttpRequests(request-> request.anyRequest().authenticated());
-        /** To Have login form**/
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        /** To make it STATELESS**/
-        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        /*lambda approach
+         * To diable CSRF token
+         *  To Authorize all the requests
+         * To Have login form
+         * To make it STATELESS
+         * */
+        return http.csrf(custom->custom.disable())
+                .authorizeHttpRequests(request-> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
 
-       return http.build();
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("Saumya")
+                .password("S@123")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("Sakshi")
+                .password("A@123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
