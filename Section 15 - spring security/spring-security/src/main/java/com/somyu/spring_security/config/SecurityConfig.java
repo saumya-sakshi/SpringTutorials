406065c35ec6a62dcdee7cfc8eb02b4d1100ce3b
Saumya Sakshi <saumyasakshi41@gmail.com>
2025-11-22 18:17:25 +0530

    Spring Security custom filter with lambda approach

--- Section 15 - spring security/spring-security/src/main/java/com/somyu/spring_security/config/SecurityConfig.java
+++ Section 15 - spring security/spring-security/src/main/java/com/somyu/spring_security/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.somyu.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +15,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //lambda approach
+        /** To diable CSRF token**/
+        http.csrf(custom->custom.disable());
+        /** To Authorize all the requests**/
+        http.authorizeHttpRequests(request-> request.anyRequest().authenticated());
+        /** To Have login form**/
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        /** To make it STATELESS**/
+        http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
        return http.build();
     }
