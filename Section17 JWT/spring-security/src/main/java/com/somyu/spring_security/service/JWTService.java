cd880634887bf2d00d352b561f32d76a7d0ebf5b
Saumya Sakshi <saumyasakshi41@gmail.com>
2026-01-05 22:58:10 +0530

    implementing JWT filter and handling the token from server side

--- Section17 JWT/spring-security/src/main/java/com/somyu/spring_security/service/JWTService.java
+++ Section17 JWT/spring-security/src/main/java/com/somyu/spring_security/service/JWTService.java
@@ -1,22 +1,56 @@
 package com.somyu.spring_security.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
+
+import static io.jsonwebtoken.Jwts.*;
 
 @Service
 public class JWTService {
+
+    private static  final String SECRET ="drlFGHkgthdrjhgfwtkewkrtwDTHthiwehtih4whkwqETHeioh4wtibegbwk=\r\n";
+
+//    another approach to generate secret key
+
+    private String secretKey;
+
+    public JWTService(){
+        secretKey= generateKey();
+    }
+
+    public String generateKey(){
+        try{
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey secretKey = keyGen.generateKey();
+            System.out.println("Secret Key: " + secretKey.toString());
+            return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        }catch(NoSuchAlgorithmException e){
+            throw new RuntimeException("Error generating secret Key "+e);
+        }
+    }
+
+
     public String generateToken(String username) {
 
     Map<String, Object> claims = new HashMap<String, Object>();
     claims.put("username", username);
 
-    return Jwts.builder()
+    return builder()
             .setClaims(claims)
             .setSubject(username)
             .setIssuedAt( new Date(System.currentTimeMillis()))
@@ -26,7 +60,35 @@ public class JWTService {
     }
 
     private Key getKey() {
-//        TO DO
-        return null;
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUserName(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+       final String userName = extractUserName(token);
+       return (userName.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    private<T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(getKey())
+                .build().parseClaimsJws(token).getBody();
     }
 }
