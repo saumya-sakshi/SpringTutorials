a7053125e079c327492411cd1cce9cedfbd8abab
Saumya Sakshi <saumyasakshi41@gmail.com>
2026-01-10 16:54:59 +0530

    Monolithic Architecture

--- Microservices/quizapp/src/main/java/com/somyu/quizapp/repository/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package com.somyu.quizapp.repository;
-
-import com.somyu.quizapp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepo extends JpaRepository<Quiz, Integer> {
-}
